d4dc81afd964332c0e53fa9d6e0db77811284e78
cnreh1986 <cnreh1980@gmail.com>
2024-09-29 15:14:26 +0530

    Toast

--- todolist/script.js
+++ todolist/script.js
@@ -28,17 +28,40 @@ function addTask() {
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
+    showToast("Task status updated!", "fas fa-info-circle icon-info");
   }
   inputBox.value = "";
 }
+// toast
+function showToast(message, iconClass) {
+  const toastIcon = document.getElementById("toast-icon");
+  const toastMessage = document.getElementById("toast-message");
 
+  toastIcon.className = iconClass; // Set the icon class
+  toastMessage.innerHTML = message; // Set the message
+
+  const toast = document.getElementById("toast");
+  toast.className = "toast show";
+
+  // Hide the toast after 3 seconds
+  setTimeout(function () {
+    toast.className = toast.className.replace("show", "");
+  }, 3000);
+}
+
+// click
 listcontainer.addEventListener(
   "click",
   function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
+      showToast("Items selected!", "fas fa-check-circle icon-info()");
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
+      showToast(
+        "Items removed successfully!",
+        "fas fa-check-circle icon-success"
+      );
     }
   },
   false
